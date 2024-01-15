819eea7e95c08efec55075517cebeef3215ee199
반주현 <qkswngus@gmail.com>
2024-01-15 12:47:55 +0900

    project init

---
+++ backend/src/main/java/com/potato/TutorCall/TutorCallApplication.java
@@ -0,0 +1,13 @@
+package com.potato.TutorCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorCallApplication.class, args);
+	}
+
+}
