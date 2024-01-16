20b53053f8dd40f6bab45f3ed0bc538e75b75f82
qudwn2941 <qudwn2941@naver.com>
2024-01-16 15:14:54 +0900

    q&a 게시판 생성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import NavBar from '@/components/NavBar.vue'
+import QA from '@/pages/board/QA.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <NavBar />
+  <!-- <div class="container"> -->
+  <QA />
+  <!-- </div> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 20px; /* 양 끝에 20px 간격을 주는 예제 값 */
+}
+</style>
