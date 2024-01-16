9607fec1b77e52c344176f02d0e38536cf662e3f
jaechan <penguin_03@naver.com>
2024-01-16 13:02:32 +0900

    frontend add sidebar design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
+import SideBar from '@/pages/tutorcall/SideBar.vue'
 </script>
 
 <template>
   <NavBar />
+  <SideBar />
 </template>
 
 <style scoped></style>
