c764e547683847e197813dce832d3c9a21025162
반주현 <qkswngus@gmail.com>
2024-01-17 16:44:33 +0900

    Design: 튜터 메인페이지 컴포넌트 구현
    Related to: S10P12A604-89

--- frontend/src/pages/mainpage/RecommendedTutor.vue
+++ frontend/src/pages/mainpage/RecommendedTutor.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <img src="https://via.placeholder.com/200x300" class="w-full h-auto opacity-75" />
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/pages/mainpage/TutorIndex.vue
+++ frontend/src/pages/mainpage/TutorIndex.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        <AboutPage />
+  <div class="bg-gray-200 font-sans font-thin px-6 bg-fixed bg-cover bg-no-repeat">
+    <div class="mx-auto max-w-6xl bg-white py-10 px-6 lg:px-12 shadow-xl mb-24">
+      <ReviewCarousel class="mt-10" />
+      <AboutPage class="my-10" />
+      <FindTutor />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import AboutPage from './AboutPage.vue';
-
-
+import AboutPage from './AboutPage.vue'
+import FindTutor from './FindTutor.vue'
+import ReviewCarousel from './ReviewCarousel.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
