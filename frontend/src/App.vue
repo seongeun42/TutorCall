c4da7692cb4b6114c71dff6a5e3f6c0381925fbf
qudwn2941 <qudwn2941@naver.com>
2024-01-17 17:59:40 +0900

    FAQ 게시판 생성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,7 @@ import LectureRecruit from '@/pages/board/lecturerecruiting/LectureRecruit.vue'
 import QA from '@/pages/board/q&a/QA.vue'
 import DetailQA from '@/pages/board/q&a/DetailQA.vue'
 import DetailLecture from './pages/board/lecturerecruiting/DetailLecture.vue'
+import Notice from './pages/board/notice/Notice.vue'
 </script>
 
 <template>
@@ -11,7 +12,8 @@ import DetailLecture from './pages/board/lecturerecruiting/DetailLecture.vue'
   <!-- <QA /> -->
   <!-- <DetailQA /> -->
   <!-- <LectureRecruit /> -->
-  <DetailLecture />
+  <!-- <DetailLecture /> -->
+  <Notice />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/pages/board/notice/FAQArticle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="border border-gray-200 py-10 my-10 p-20">
+    <div class="flex">
+      <div class="">
+        <p class="font-bold text-xl mr-20">12</p>
+      </div>
+      <div class="flex-1">
+        <div class="flex">
+          <p class="w-20 rounded-lg bg-blue-800 text-white text-center">Q</p>
+          <a class="font-bold text-xl cursor-pointer ml-10" @click="toggleAnswer">
+            회원 가입 시, 오류가 발생합니다.
+          </a>
+        </div>
+        <div v-if="showAnswer" class="flex my-10">
+          <p class="w-20 rounded-lg bg-red-600 text-white text-center">A</p>
+          <p class="font-bold text-xl ml-10">
+            알아서 잘 딱 깔끔하고 센스있게 하시는 방법이 있습니다.
+          </p>
+        </div>
+      </div>
+      <div>12.12.12</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const showAnswer = ref(false)
+
+const toggleAnswer = () => {
+  showAnswer.value = !showAnswer.value
+}
+</script>
+
+<style scoped></style>
