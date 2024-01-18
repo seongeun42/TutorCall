5db82fae19842434f1142100ee27656f967cf9cf
sikutisa <ilgooinkorea@gmail.com>
2024-01-18 15:53:46 +0900

    Fix merge confilct

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,14 @@ import NavBar from '@/components/NavBar.vue'
 
 import StudentIndex from '@/pages/mainpage/StudentIndex.vue'
 import TutorIndex from '@/pages/mainpage/TutorIndex.vue'
+import ChatRoom from '@/components/ChatRoom.vue'
 </script>
 
 <template>
   <NavBar />
   <!-- <StudentIndex /> -->
   <TutorIndex />
+  <ChatRoom />
 </template>
 
 <style scoped>
