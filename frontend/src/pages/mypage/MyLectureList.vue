b767306d0d2a64dbb493fb81f0af2c733a223927
qudwn2941 <qudwn2941@naver.com>
2024-01-18 17:52:16 +0900

    Design: 선생 Mypage 내 과외 목록 조회 페이지 생성
    Related to: S10P12A604-108

---
+++ frontend/src/pages/mypage/MyLectureList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex flex-wrap justify-around">
+    <div v-for="index in 8" :key="index" class="mb-4">
+      <div
+        class="w-250 h-200 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+      >
+        <div
+          class="relative overflow-hidden bg-cover bg-no-repeat"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+        >
+          <a href="#!">
+            <div
+              class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+            ></div>
+          </a>
+        </div>
+        <div class="p-6 mb-10">
+          <div class="flex mb-10">
+            <div class="bg-blue-300 p-2 rounded-xl aspect-w-2 aspect-h-1 mr-2">
+              <p class="font-bold">프로그래밍</p>
+            </div>
+            <div class="bg-green-300 p-2 rounded-xl aspect-w-2 aspect-h-1">
+              <p class="font-bold">수요일 17:00</p>
+            </div>
+          </div>
+          <div class="flex justify-center">
+            <img src="@/img/spring.png" alt="" class="w-80 h-56" />
+          </div>
+          <h5
+            class="text-center mb-2 text-2xl font-bold leading-tight text-neutral-800 dark:text-neutral-50"
+          >
+            스프링 핵심 원리 - 기본편
+          </h5>
+
+          <div class="flex items-center my-8 justify-end">
+            <img src="@/img/teacher.png" alt="" class="w-10 h-10 rounded-full" />
+            <p>한석원</p>
+          </div>
+          <div class="flex justify-end">
+            <button
+              type="button"
+              class="text-white mx-2 mb-3 inline-block rounded bg-blue-900 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              참가하기
+            </button>
+            <button
+              type="button"
+              class="text-white mb-3 inline-block rounded bg-green-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              리뷰 남기기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- frontend/src/pages/mypage/MyPage.vue
+++ frontend/src/pages/mypage/MyPage.vue
@@ -3,13 +3,16 @@
     <SideBar />
     <div class="flex-1 bg-blue-100 p-4">
       <p class="text-4xl font-bold my-10 mx-32">{{ title }}</p>
-      <div class="container p-40 bg-white"></div>
+      <div class="container p-40 bg-white">
+        <MyLectureList />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import SideBar from '@/components/SideBar.vue'
+import MyLectureList from './MyLectureList.vue'
 import type { Ref } from 'vue'
 import { ref } from 'vue'
 
