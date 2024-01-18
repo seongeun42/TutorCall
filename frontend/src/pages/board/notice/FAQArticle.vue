89be6f55a1c2a9eee5082a7c9d3e0e3601d7703f
qudwn2941 <qudwn2941@naver.com>
2024-01-18 11:12:03 +0900

    Design: 공지사항 상세 보기 페이지 생성
    Related to: S10P12A604-93

--- frontend/src/pages/board/notice/FAQArticle.vue
+++ frontend/src/pages/board/notice/FAQArticle.vue
@@ -6,19 +6,29 @@
       </div>
       <div class="flex-1">
         <div class="flex">
-          <p class="w-20 rounded-lg bg-blue-800 text-white text-center">Q</p>
-          <a class="font-bold text-xl cursor-pointer ml-10" @click="toggleAnswer">
+          <p
+            class="rounded-lg bg-blue-800 text-white text-center white-space: nowrap;"
+            style="width: 100px; height: 30px"
+          >
+            Q
+          </p>
+          <p class="font-bold text-xl cursor-pointer ml-10 flex-1" @click="toggleAnswer">
             회원 가입 시, 오류가 발생합니다.
-          </a>
+          </p>
         </div>
         <div v-if="showAnswer" class="flex my-10">
-          <p class="w-20 rounded-lg bg-red-600 text-white text-center">A</p>
-          <p class="font-bold text-xl ml-10">
+          <p
+            class="rounded-lg bg-red-600 text-white text-center"
+            style="width: 100px; height: 30px"
+          >
+            A
+          </p>
+          <p class="font-bold text-xl ml-10 flex-1">
             알아서 잘 딱 깔끔하고 센스있게 하시는 방법이 있습니다.
           </p>
         </div>
       </div>
-      <div>12.12.12</div>
+      <div class="ml-5">12.12.12</div>
     </div>
   </div>
 </template>
---
+++ frontend/src/pages/board/notice/MenuBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex justify-center mt-8">
+    <button
+      type="button"
+      class="rounded bg-blue-100 mr-6 px-6 py-3 pb-2 pt-2 text-2xl font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+    >
+      공지사항
+    </button>
+    <button
+      type="button"
+      class="rounded bg-green-100 px-6 py-3 pb-2 pt-2 text-2xl font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+    >
+      FAQ
+    </button>
+  </div>
+  <div class="flex justify-end items-center mt-10">
+    <select
+      class="p-2 border border-gray-300 rounded-md mr-1 appearance-none"
+      v-model="selectedSubject"
+    >
+      <option value="">정렬</option>
+      <option value="math">오름차순</option>
+      <option value="science">내림차순</option>
+      <!-- 다른 과목들도 추가할 수 있습니다. -->
+    </select>
+
+    <input
+      type="text"
+      placeholder="search for keywords"
+      class="w-60 p-2 border border-gray-300 rounded-md mr-1"
+    />
+    <button
+      type="button"
+      class="px-4 py-2 bg-gray-400 hover:bg-gray-500 rounded-md text-white mr-3"
+    >
+      검색
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const selectedSubject = ref('')
+</script>
+
+<style scoped></style>
