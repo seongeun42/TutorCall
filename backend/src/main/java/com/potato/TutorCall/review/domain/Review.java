032afbe0160ec4dc7363b21d7ebc4365a17e8e0f
seongeun <gkapf838@gmail.com>
2024-01-22 15:54:42 +0900

    Feat: Review Domain create Related to: S10P12A604-171

---
+++ backend/src/main/java/com/potato/TutorCall/review/domain/Review.java
@@ -0,0 +1,44 @@
+package com.potato.TutorCall.review.domain;
+
+import com.potato.TutorCall.lecture.domain.Lecture;
+import com.potato.TutorCall.tutor.domain.Tutor;
+import com.potato.TutorCall.tutorcall.domain.TutorCall;
+import com.potato.TutorCall.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Tutor tutor;
+
+    private User reviewer;
+
+    private Lecture lecture;
+
+    private TutorCall tutorCall;
+
+    private int mannerRate;
+
+    private int communicationRate;
+
+    private int professionalismRate;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
