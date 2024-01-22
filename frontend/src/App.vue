350f4b976155aecef202325d89f4f7e077e2938f
박연서 <qkrdustj45@naver.com>
2024-01-22 17:54:20 +0900

    Design: Nav의 알림 버튼에 dropdown 추가, tutor call 알림 페이지 추가

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+
+</script>
+
+<template>
+  <NavBar />
+
+</template>
+
+<style scoped>
+.test {
+  height: 100%;
+}
+</style>
