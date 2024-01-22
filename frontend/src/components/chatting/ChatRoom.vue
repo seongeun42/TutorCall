61ac7f184935b700b32a5879ebe44793077bbce6
sikutisa <ilgooinkorea@gmail.com>
2024-01-22 14:05:46 +0900

    Feat: add style to ChatRoom.vue
    Related to: S10P12A604-130

--- frontend/src/components/chatting/ChatRoom.vue
+++ frontend/src/components/chatting/ChatRoom.vue
@@ -5,11 +5,20 @@ import ChattingMessage from '@/components/chatting/ChattingMessage.vue'
 </script>
 
 <template>
-  <div id="chatbox">
-    <div id="chatview" class="p1">
+  <div
+    id="chatbox"
+    class="w-[290px] overflow-hidden h-[484px] absolute ml-[-155px] rounded-md left-2/4 top-[100px]"
+  >
+    <div
+      id="chatview"
+      class="p1 w-[290px] h-[484px] absolute left-0 top-0 overflow-hidden rounded-md"
+    >
       <ChatProfile />
       <div id="chat-messages">
-        <label>Thursday 02</label>
+        <label
+          class="text-[#aab8c2] font-semibold text-xs text-center w-[290px] block mx-0 my-[15px]"
+          >Thursday 02</label
+        >
         <ChattingMessage />
       </div>
 
