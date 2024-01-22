c235140249dd7aad1050619e873cf09c23134f93
sikutisa <ilgooinkorea@gmail.com>
2024-01-22 14:18:23 +0900

    Feat: add style to ChatProfile.vue
    Related to: S10P12A604-130

--- frontend/src/components/chatting/ChatProfile.vue
+++ frontend/src/components/chatting/ChatProfile.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="profile">
+  <div
+    id="profile"
+    class="h-[153px] overflow-hidden text-center text-white bg-[#ffffff] bg-[url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/timeline1.png')] bg-no-repeat"
+  >
     <div id="close">
       <div class="cy"></div>
       <div class="cx"></div>
     </div>
 
     <p>Miro Badev</p>
-    <span>miro@badev@gmail.com</span>
+    <span class="font-normal text-[11px]">miro@badev@gmail.com</span>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#profile .avatar {
+  @apply w-[68px] mt-[23px] mb-0 mx-0 rounded-[50%] border-[3px] border-solid border-white;
+}
+#profile p {
+  @apply font-semibold text-[15px] opacity-0 transition-all duration-200 ease-[cubic-bezier(0.000,0.995,0.990,1.000)] mt-[118px] -mb-px mx-0;
+  -webkit-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -moz-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -ms-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -o-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+}
+#profile p.animate {
+  @apply opacity-100 transition-all duration-200 ease-[cubic-bezier(0.000,0.995,0.990,1.000)] mt-[97px];
+  -webkit-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -moz-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -ms-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+  -o-transition: all 200ms cubic-bezier(0, 0.995, 0.99, 1);
+}
+</style>
