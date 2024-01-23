fa7474f6806651a71c78087624eac2d22ff1b78c
sikutisa <ilgooinkorea@gmail.com>
2024-01-23 15:44:00 +0900

    Feat: add mypage controllers
    java auto lint가 정상적으로 동작하려면, jar파일이 프로젝트 최상위 폴더에
    위치해야함
    
    상세 기능 구현은 각 api 별로 진행하는 게 맞는 것 같음
    
    Related to: S10P12A604-146

--- backend/src/main/java/com/potato/TutorCall/mypage/controller/MypageController.java
+++ backend/src/main/java/com/potato/TutorCall/mypage/controller/MypageController.java
@@ -1,7 +1,125 @@
 package com.potato.TutorCall.mypage.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.potato.TutorCall.mypage.dto.req.MyPagePaginationDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+/** 마이페이지 기능에 대한 컨트롤러 */
 @RestController
+@RequestMapping("/mypage")
+@Slf4j
 public class MypageController {
+
+  /**
+   * 내 정보 조회
+   *
+   * @return
+   */
+  @GetMapping
+  public ResponseEntity<?> getMyProfile() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 프로필 사진 변경
+   *
+   * @return
+   */
+  @PatchMapping("/profile")
+  public ResponseEntity<?> updateProfileImage() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 닉네임 수정
+   *
+   * @return
+   */
+  @PatchMapping("/nickname")
+  public ResponseEntity<?> updateNickname() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 비밀번호 변경
+   *
+   * @return
+   */
+  @PatchMapping("/password")
+  public ResponseEntity<?> updatePassword() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 알림 설정 변경
+   *
+   * @return
+   */
+  @PatchMapping("/notification")
+  public ResponseEntity<?> updateNotificationConfig() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 강의 태그 수정
+   *
+   * @return
+   */
+  @PutMapping("/tutor/tag")
+  public ResponseEntity<?> updateLectureTag() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 소개글 수정
+   *
+   * @return
+   */
+  @PatchMapping("/tutor/intro")
+  public ResponseEntity<?> updateIntroduction() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 내 쿠폰 내역 조회
+   *
+   * @param paginationOption pageNo(페이지 번호), size(한 페이지의 크기)
+   * @return
+   */
+  @GetMapping("/coupon")
+  public ResponseEntity<?> getCouponHistory(MyPagePaginationDto paginationOption) {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 내 포인트 내역 조회
+   *
+   * @param paginationOption pageNo(페이지 번호), size(한 페이지의 크기)
+   * @return
+   */
+  @GetMapping("/point")
+  public ResponseEntity<?> getPointHistory(MyPagePaginationDto paginationOption) {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 내 과외 목록 조히(끝났다면 리뷰 작성 여부 포함)
+   *
+   * @return
+   */
+  @GetMapping("/lecture")
+  public ResponseEntity<?> getLectureList() {
+    return ResponseEntity.badRequest().build();
+  }
+
+  /**
+   * 내 화상강의 내역 조회(리뷰 작성 여부 포함 - 있다면 리뷰까지)
+   *
+   * @return
+   */
+  @GetMapping("/tutorcall")
+  public ResponseEntity<?> getTutorcallHistory() {
+    return ResponseEntity.badRequest().build();
+  }
 }
