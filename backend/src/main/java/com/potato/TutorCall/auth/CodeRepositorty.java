2931c32bca979cb163e47e85fd23095ff08cf250
jaechan <penguin_03@naver.com>
2024-01-24 17:17:24 +0900

    이메일 관련

--- backend/src/main/java/com/potato/TutorCall/auth/CodeRepositorty.java
+++ backend/src/main/java/com/potato/TutorCall/auth/CodeRepositorty.java
@@ -1,2 +1,32 @@
-package com.potato.TutorCall.auth;public class CodeRepositorty {
+package com.potato.TutorCall.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class CodeRepositorty {
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String hash_key="email-";
+    private static final Long expiredTime = 300L; // 5분
+
+    //key는 EAMIL입니다.
+    public void setCode(String key, String code){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(hash_key+key, code,expiredTime , TimeUnit.SECONDS);
+    }
+
+    public String getCode(String key){
+        return (String) redisTemplate.opsForValue().get(hash_key + key);
+    }
+
+    public void deleteCode(String key){
+        redisTemplate.delete(hash_key + key);
+    }
+
 }
--- backend/src/main/java/com/potato/TutorCall/auth/dto/request/SendEmailRequestDto.java
+++ backend/src/main/java/com/potato/TutorCall/auth/dto/request/SendEmailRequestDto.java
@@ -1,2 +1,8 @@
-package com.potato.TutorCall.auth.dto.request;public class SendEmailRequestDto {
+package com.potato.TutorCall.auth.dto.request;
+
+import lombok.Data;
+
+@Data
+public class SendEmailRequestDto {
+    private String email;
 }
