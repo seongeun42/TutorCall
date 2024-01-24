51f57147f7d4f408cd32f3011648cbfbae19480b
seongeun <gkapf838@gmail.com>
2024-01-24 10:56:29 +0900

    Fix: db 예약어와 겹치는 entity 변수명 desc -> description으로 수정

--- backend/src/main/java/com/potato/TutorCall/chat/repository/ChatroomRepository.java
+++ backend/src/main/java/com/potato/TutorCall/chat/repository/ChatroomRepository.java
@@ -1,8 +1,9 @@
 package com.potato.TutorCall.chat.repository;
 
+import com.potato.TutorCall.chat.domain.Chatroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatroomRepository extends JpaRepository<Object, Long> {
+public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
 }
