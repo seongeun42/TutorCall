b167a89a8309d9bd3d1bb58144c1fb5f3b18774b
seongeun <gkapf838@gmail.com>
2024-01-25 13:40:12 +0900

    Feat: Tag, TutorTag 저장 함수 추가

---
+++ backend/src/main/java/com/potato/TutorCall/tutor/service/TagService.java
@@ -0,0 +1,18 @@
+package com.potato.TutorCall.tutor.service;
+
+import com.potato.TutorCall.tutor.domain.Tag;
+import com.potato.TutorCall.tutor.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+
+    public Long save(Tag tag) {
+        return tagRepository.save(tag).getId();
+    }
+
+}
