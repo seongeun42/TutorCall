befdef851998219ed3d876cdfb296181d317aa56
seongeun <gkapf838@gmail.com>
2024-01-25 14:59:47 +0900

    Refactor: MypageService의 유저 정보 조회 함수 리팩토링 Related to: S10P12A604-161

--- backend/src/main/java/com/potato/TutorCall/mypage/service/MypageService.java
+++ backend/src/main/java/com/potato/TutorCall/mypage/service/MypageService.java
@@ -2,12 +2,16 @@ package com.potato.TutorCall.mypage.service;
 
 import com.potato.TutorCall.exception.customException.NotFoundException;
 import com.potato.TutorCall.mypage.dto.res.MyPageProfileResDto;
+import com.potato.TutorCall.tutor.domain.Tag;
 import com.potato.TutorCall.tutor.domain.Tutor;
 import com.potato.TutorCall.tutor.domain.TutorTag;
 import com.potato.TutorCall.tutor.repository.TutorRepository;
 import com.potato.TutorCall.tutor.repository.TutorTagRepository;
+import com.potato.TutorCall.tutor.service.TutorService;
 import com.potato.TutorCall.user.domain.User;
+import com.potato.TutorCall.user.domain.enums.RoleType;
 import com.potato.TutorCall.user.repository.UserRepository;
+import com.potato.TutorCall.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,20 +24,23 @@ import java.util.Optional;
 public class MypageService {
 
     private final UserRepository userRepository;
-    private final TutorRepository tutorRepository;
-    private final TutorTagRepository tutorTagRepository;
+    private final TutorService tutorService;
 
     public MyPageProfileResDto getUserProfile(Long id) {
         User currentUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("사용자 정보가 없습니다."));
 
-        MyPageProfileResDto userInfo = new MyPageProfileResDto(currentUser);
-
-        // 유저가 선생이라면
-        Optional<Tutor> currentTutor = tutorRepository.findByUser(currentUser);
-        if(currentTutor.isPresent()) {
-            List<TutorTag> tutorTagList = tutorTagRepository.findByTutor(currentTutor.get());
-
-            userInfo.addTutorInfo(currentTutor.get(), tutorTagList);
+        MyPageProfileResDto userInfo = null;
+        if (currentUser.getRole() == RoleType.USER)
+            userInfo = MyPageProfileResDto.builder()
+                    .user(currentUser)
+                    .build();
+        else {
+            Tutor tutor = tutorService.findById(currentUser.getId());
+            List<Tag> tags = tutorService.getTutorTags(tutor);
+            userInfo = MyPageProfileResDto.builder()
+                    .user(currentUser)
+                    .tutor(tutor)
+                    .build();
         }
 
         return userInfo;
