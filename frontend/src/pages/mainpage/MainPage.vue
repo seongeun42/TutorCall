5023b1b834321bc7d333da1ffc87ef48d946a69b
반주현 <qkswngus@gmail.com>
2024-01-26 12:20:47 +0900

    Feat: Vue Router 설계
    Related to: S10P12A604-276

---
+++ frontend/src/pages/mainpage/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-gray-200 font-sans font-thin px-6 bg-fixed bg-cover bg-no-repeat">
+    <div class="mx-auto max-w-6xl bg-white py-20 px-6 lg:px-12 shadow-xl mb-24">
+      <template v-if="!isTutor"
+        ><ProblemUpload class="mb-10" />
+        <AboutPage class="mb-10" />
+        <ReviewCarousel
+      /></template>
+      <template v-if="isTutor"
+        ><ReviewCarousel class="mt-10" />
+        <AboutPage class="my-10" />
+        <FindTutor
+      /></template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AboutPage from './AboutPage.vue'
+import ProblemUpload from './ProblemUpload.vue'
+import ReviewCarousel from './ReviewCarousel.vue'
+import FindTutor from './FindTutor.vue'
+import { ref } from 'vue'
+
+// 추후 로직 구현 필요(비회원 & 학생 / 선생님 회원 구분)
+const isTutor = ref(false)
+</script>
+
+<style scoped></style>
