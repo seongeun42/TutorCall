35ba3c8534eb460bdff78eaaa77ceb321bd7274c
qudwn2941 <qudwn2941@naver.com>
2024-01-29 13:39:22 +0900

    Fix: Navbar 3개로 구분(비로그인, 학생, 선생)
    Related to: S10P12A604-296

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { ref, type Ref } from 'vue'
 import NavBar from '@/components/NavBar.vue'
 import MainPage from '@/pages/mainpage/MainPage.vue'
+import Mypage from '@/pages/mypage/MyPage.vue'
+import StudentNavBar from './components/StudentNavBar.vue'
+import NotLoginNavBar from './components/NotLoginNavBar.vue'
+const isTutor: Ref<boolean> = ref(false)
+const isLogin: Ref<boolean> = ref(false)
 </script>
 
 <template>
-  <NavBar />
-  <MainPage />>
+  <div v-if="!isLogin">
+    <NotLoginNavBar />
+  </div>
+  <div v-else>
+    <NavBar v-if="isTutor" />
+    <StudentNavBar v-else />
+  </div>
+  <!-- <MainPage /> -->
+  <Mypage />
 </template>
 
 <style scoped></style>
