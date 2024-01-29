b25138250267545fcd4a2edc898e486ca0d5bb1c
박연서 <qkrdustj45@naver.com>
2024-01-29 16:36:10 +0900

    Fix: conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
-<<<<<<< HEAD
 import { RouterView } from "vue-router";
 
+import StudentIndex from '@/pages/mainpage/StudentIndex.vue'
+import TutorIndex from '@/pages/mainpage/TutorIndex.vue'
+import MyPage from './pages/mypage/MyPage.vue'
 </script>
 
 <template>
   <NavBar />
   <Router-View id="main" />
-=======
-import OnlineLecture from '@/pages/onlinelecture/OnlineLecture.vue'
-import ChatBox from './components/chatting/ChatBox.vue'
-</script>
-
-<template>
-  <!-- <NavBar /> -->
-  <!-- <OnlineLecture/> -->
-  <ChatBox />
->>>>>>> frontend
 </template>
 
 <style scoped>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue'
 import CallNotification from '@/components/CallNotification.vue'
-const isTutor: Ref<boolean> = ref(true)
 
 interface notifyDate {
   message: string
@@ -22,51 +21,10 @@ function handleMode(show: boolean, mode: string) {
   <nav class="bg-white">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <button
-            type="button"
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-          >
-            <span class="absolute -inset-0.5"></span>
-            <span class="sr-only">Open main menu</span>
-            <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-            <svg
-              class="block h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-            <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-
-            <img class="hidden h-6 w-6" src="@/img/setting.png" />
-          </button>
-        </div>
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-<<<<<<< HEAD
             <img class="h-8 w-auto" src="@/img/logo.png" alt="Your Company" />
-=======
-            <img class="h-8 w-auto" src="../img/logo.png" alt="Your Company" />
->>>>>>> frontend
           </div>
           <div class="hidden sm:ml-6 sm:block flex-1 justify-evenly">
             <div class="flex space-x-4 justify-evenly">
@@ -87,7 +45,7 @@ function handleMode(show: boolean, mode: string) {
                 class="font-semibold inline-block text-black-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm"
                 >공지사항</a
               >
-              <div v-if="isTutor" class="flex items-center">
+              <div class="flex items-center">
                 <p class="ms-3 text-sm font-semibold text-black-500 dark:text-gray-300">
                   화상과외 활성화
                 </p>
@@ -137,37 +95,6 @@ function handleMode(show: boolean, mode: string) {
               </ul>
             </div>
           </div>
-          <!-- 설정 톱니바퀴 버튼 만들기 -->
-          <button
-            type="button"
-            class="ml-3 relative rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span class="absolute -inset-1.5"></span>
-            <span class="sr-only">Settings</span>
-            <img src="@/img/setting.png" alt="" class="w-6 h-6" />
-          </button>
-          <!-- 설정 톱니바퀴 버튼 만들기 -->
-          <!-- <button
-            type="button"
-            class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span class="absolute -inset-1.5"></span>
-            <span class="sr-only">View notifications</span>
-            <svg
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-              />
-            </svg>
-          </button> -->
 
           <!-- Profile dropdown -->
           <div class="relative ml-3">
