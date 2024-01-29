c2b58786f72aae4362f2c8c28a80ff9e12efd1fd
jaechan <penguin_03@naver.com>
2024-01-29 09:47:44 +0900

    Fix:세션 레디스 설정 임시제거

--- backend/src/main/java/com/potato/TutorCall/TutorCallApplication.java
+++ backend/src/main/java/com/potato/TutorCall/TutorCallApplication.java
@@ -3,11 +3,9 @@ package com.potato.TutorCall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableRedisHttpSession
 public class TutorCallApplication {
 
   public static void main(String[] args) {
