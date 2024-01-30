0484b73e5d3e2ed21c1cee4636e027c53f18dac3
jaechan <penguin_03@naver.com>
2024-01-30 15:45:59 +0900

    UserDto 수정

--- backend/src/main/java/com/potato/TutorCall/user/dto/UserDto.java
+++ backend/src/main/java/com/potato/TutorCall/user/dto/UserDto.java
@@ -1,6 +1,8 @@
 package com.potato.TutorCall.user.dto;
 
 
+import com.potato.TutorCall.user.domain.User;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -9,4 +11,12 @@ public class UserDto {
     private String nickname;
     private String profile;
     private Integer point;
+
+    @Builder
+    public UserDto(Long userId, String nickname, String profile, Integer point) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.profile = profile;
+        this.point = point;
+    }
 }
