7b0209d8262dfe04b53bcd2e4bf85c90361addd6
jaechan <penguin_03@naver.com>
2024-01-31 17:29:43 +0900

    create oauth dto

--- backend/src/main/java/com/potato/TutorCall/auth/dto/oauth/OAuth2LoginUserKakaoDto.java
+++ backend/src/main/java/com/potato/TutorCall/auth/dto/oauth/OAuth2LoginUserKakaoDto.java
@@ -1,2 +1,26 @@
-package com.potato.TutorCall.auth.dto;public class OAuth2LoginUserKakaoDto {
+package com.potato.TutorCall.auth.dto.oauth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OAuth2LoginUserKakaoDto implements OAuth2LoginUserDto<CommonInfo>{
+    private Map<String,Object> attributes = new HashMap<>(); // 받은거
+    private CommonInfo commonInfo;
+
+    @Override
+    public CommonInfo getAttribute() {
+        return this.commonInfo;
+    }
+
+    @Override
+    public CommonInfo toEntity() {
+//        Map<String, Object> obj = attributes.get("response");
+//
+//        ObjectMapper
+
+        return null;
+    }
+
 }
