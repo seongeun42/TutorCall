a19ed6fca9d561ee735a23af758b1b9567743ce9
jaechan <penguin_03@naver.com>
2024-01-31 09:07:56 +0900

    OAuth2UserCustom생성

--- backend/src/main/java/com/potato/TutorCall/auth/dto/oauth/CustomOAuth2User.java
+++ backend/src/main/java/com/potato/TutorCall/auth/dto/oauth/CustomOAuth2User.java
@@ -1,4 +1,4 @@
-package com.potato.TutorCall.auth.dto;
+package com.potato.TutorCall.auth.dto.oauth;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
