5c984981b2dc0e4310075a234dc3d2f741df79e3
sikutisa <ilgooinkorea@gmail.com>
2024-02-01 16:20:38 +0900

    Fix: fix crudrepository interface

--- backend/src/main/java/com/potato/TutorCall/chat/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/potato/TutorCall/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,8 @@
 package com.potato.TutorCall.chat.repository;
 
 import com.potato.TutorCall.chat.domain.ChatMessage;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatMessageRepository extends ListCrudRepository<ChatMessage, Long> {}
+public interface ChatMessageRepository extends ReactiveCrudRepository<ChatMessage, Long> {}
--- backend/src/main/java/com/potato/TutorCall/chat/repository/ChatroomRepository.java
+++ backend/src/main/java/com/potato/TutorCall/chat/repository/ChatroomRepository.java
@@ -1,8 +1,8 @@
 package com.potato.TutorCall.chat.repository;
 
 import com.potato.TutorCall.chat.domain.Chatroom;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatroomRepository extends ListCrudRepository<Chatroom, Long> {}
+public interface ChatroomRepository extends ReactiveCrudRepository<Chatroom, Long> {}
