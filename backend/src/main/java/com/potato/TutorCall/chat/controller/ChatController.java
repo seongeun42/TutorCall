56ebb19945cb0d002bf2e3a88260e622a7eb1f38
sikutisa <ilgooinkorea@gmail.com>
2024-02-01 15:56:53 +0900

    Feat: add chat feature skeleton
    Related to: S10P12A604-257

--- backend/src/main/java/com/potato/TutorCall/chat/controller/ChatController.java
+++ backend/src/main/java/com/potato/TutorCall/chat/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.potato.TutorCall.chat.controller;
 
+import com.potato.TutorCall.auth.SessionKey;
+import com.potato.TutorCall.auth.dto.UserSessionDto;
 import com.potato.TutorCall.chat.dto.req.SendChatReq;
 import com.potato.TutorCall.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/** 개별 채팅 메시지에 대한 컨트롤러 */
 @RestController
 @RequestMapping("/chat")
 @RequiredArgsConstructor
@@ -17,22 +20,28 @@ public class ChatController {
   /**
    * 특정 채팅방에 메시지 불러오기
    *
+   * @param userSession 유저의 로그인 세션
    * @param roomId 채팅방의 Id
    * @return
    */
   @GetMapping(value = "/{roomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<?> receiveChatsInRoom(@PathVariable Long roomId) {
+  public Flux<?> receiveChatsInRoom(
+      @SessionAttribute(name = SessionKey.USER) UserSessionDto userSession,
+      @PathVariable Long roomId) {
     return null;
   }
 
   /**
    * 메시지전송
    *
+   * @param userSession 유저의 로그인 세션
    * @param newChat 새로운 메시지
    * @return
    */
   @PostMapping
-  public Mono<?> sendChatToRoom(@RequestBody SendChatReq newChat) {
+  public Mono<?> sendChatToRoom(
+      @SessionAttribute(name = SessionKey.USER) UserSessionDto userSession,
+      @RequestBody SendChatReq newChat) {
     return null;
   }
 }
