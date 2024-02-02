e1c228644c6e58ea917c321b42d03e12f150ccd7
박연서 <qkrdustj45@naver.com>
2024-02-02 14:04:10 +0900

    Feat: 태그 및 검색어 검색 api 연동

--- frontend/src/pages/board/q&a/DetailQA.vue
+++ frontend/src/pages/board/q&a/DetailQA.vue
@@ -124,7 +124,7 @@ function goList():void{
     <div class="mt-10 mx-40 flex justify-center items-center">
       <button
         type="button"
-        class="items-center rounded bg-primary mt-4 px-6 py-3 pb-2 pt-2.5 text-2xl font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+        class="bg-sky-500 text-white text-xl rounded-lg shadow-xl"
         style="width: calc(3 * 3rem); height: 3rem"
         @click="goList"
       >
