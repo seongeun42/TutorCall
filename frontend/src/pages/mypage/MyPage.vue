0b5e5e9c1425ead59c090c37feaaed4e078a2535
반주현 <qkswngus@gmail.com>
2024-02-02 10:57:27 +0900

    Fix: MyPage 초기화면 수정
    Related to: S10P12A604-286

--- frontend/src/pages/mypage/MyPage.vue
+++ frontend/src/pages/mypage/MyPage.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="bg-blue-50">
     <div class="flex p-20">
-      <SideBar />
+      <SideBar v-if="isTutor" />
+      <StudentSideBar v-if="!isTutor" />
       <div class="p-4 pl-20">
         <p class="text-4xl font-bold mb-10">{{ title }}</p>
         <div class="container p-12 bg-white">
-          <!-- <MyLectureList /> -->
-          <StudentMyLecture />
-          <!-- <InformationUpdate /> -->
-          <!-- <MyPaymentInfo /> -->
-          <!-- <NewPaymentAdd /> -->
-          <!-- <ReviewCheck /> -->
-          <!-- <WithdrawlPage /> -->
-          <!-- <ProfitCheck /> -->
-          <!-- <StudentInformationUpdate /> -->
+          <StudentInformationUpdate v-if="!isTutor" />
+          <InformationUpdate v-if="isTutor" />
         </div>
       </div>
     </div>
@@ -22,17 +16,13 @@
 
 <script setup lang="ts">
 import SideBar from '@/components/SideBar.vue'
-import MyPaymentInfo from '@/pages/mypage/payment/MyPaymentInfo.vue'
-import NewPaymentAdd from '@/pages/mypage/payment/NewPaymentAdd.vue'
+import StudentSideBar from '@/components/StudentSideBar.vue'
 import { ref, type Ref } from 'vue'
-import StudentInformationUpdate from './student/information/StudentInformationUpdate.vue'
-import MyLectureList from './tutor/MyLectureList.vue'
-import InformationUpdate from './tutor/InformationUpdate.vue'
-import ReviewCheck from './tutor/ReviewCheck.vue'
-import WithdrawlPage from './tutor/WithdrawlPage.vue'
-import ProfitCheck from './tutor/ProfitCheck.vue'
-import StudentMyLecture from './student/information/StudentMyLecture.vue'
+import StudentInformationUpdate from '@/pages/mypage/student/information/StudentInformationUpdate.vue'
+import InformationUpdate from '@/pages/mypage/tutor/InformationUpdate.vue'
+
 const title: Ref<String> = ref('개인정보 수정')
+const isTutor: Ref<boolean> = ref(true)
 </script>
 
 <style scoped>
