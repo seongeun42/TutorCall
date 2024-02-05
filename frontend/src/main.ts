01e92233947f0fdeab94a9b64d27b79a29867cef
qudwn2941 <qudwn2941@naver.com>
2024-02-05 12:45:48 +0900

    Fix: CKeditor 연결 main.ts 수정

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,11 +5,13 @@ import './style.css'
 import './ckeditor-reset.css'
 import App from './App.vue'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import CKEditor from '@ckeditor/ckeditor5-vue'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
+app.use(CKEditor)
 app.use(router)
 app.use(pinia)
 
