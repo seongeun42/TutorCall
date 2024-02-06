e95302f90719192e4f8711b1cf5913963868eb9c
sikutisa <ilgooinkorea@gmail.com>
2024-02-06 15:03:45 +0900

    Fix: remove frontend

--- frontend/src/axios/axiosCofig.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const instance = axios.create({
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  }
-})
--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  // eslint-disable-next-line no-undef
-  plugins: [require('@tailwindcss/nesting'), require('daisyui')]
-}
