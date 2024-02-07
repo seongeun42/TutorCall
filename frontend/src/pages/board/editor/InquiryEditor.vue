cfa55972ec2a43239ce2d5be91a8482720aabb50
qudwn2941 <qudwn2941@naver.com>
2024-02-07 09:21:13 +0900

    Feat: 관리자 문의 에디터 생성

---
+++ frontend/src/pages/board/editor/InquiryEditor.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue'
+import CkEditor from './CkEditor.vue'
+const title: Ref<string> = ref('')
+const editorData: Ref<string> = ref('')
+
+function handleModelValueUpdate(newValue: string) {
+  editorData.value = newValue
+  console.log(editorData.value)
+}
+</script>
+<template>
+  <div class="mx-auto w-[1000px]">
+    <div class="my-10 font-bold text-2xl">관리자 문의</div>
+    <div class="my-10">
+      <div class="flex items-center">
+        <input class="title" type="text" v-model="title" />
+      </div>
+    </div>
+    <CkEditor @update:modelValue="handleModelValueUpdate" />
+    <div class="my-10 flex justify-center items-center">
+      <button
+        type="button"
+        class="mr-6 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      >
+        취소
+      </button>
+      <button
+        type="button"
+        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-white focus:outline-none bg-blue-600 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      >
+        등록
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  margin: 0 10px;
+  border: 1px solid;
+  border-color: rgb(192, 192, 192);
+  border-radius: 8px;
+  width: 100%;
+  height: 40px;
+}
+</style>
