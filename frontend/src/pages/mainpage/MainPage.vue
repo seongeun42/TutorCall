6123d6728dd628e183a66ab7774be548b0950d67
반주현 <qkswngus@gmail.com>
2024-02-09 10:23:03 +0900

    Fix: 메인페이지 버튼 일부 수정 및 에디터와 연결, 라우터 수정
    Related to: S10P12A604-282, S10P12A604-285

--- frontend/src/pages/mainpage/MainPage.vue
+++ frontend/src/pages/mainpage/MainPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-gray-200 font-sans font-thin px-6 bg-fixed bg-cover bg-no-repeat">
     <div class="mx-auto max-w-6xl bg-white py-20 px-6 lg:px-12 shadow-xl mb-24">
-      <template v-if="!isTutor"
+      <template v-if="!userStore.isTutor"
         ><ProblemUpload class="mb-10" />
         <AboutPage class="mb-10" />
         <ReviewCarousel
       /></template>
-      <template v-if="isTutor"
+      <template v-if="userStore.isTutor"
         ><ReviewCarousel class="mt-10" />
         <AboutPage class="my-10" />
         <FindTutor
@@ -16,15 +16,13 @@
 </template>
 
 <script setup lang="ts">
-import AboutPage from './AboutPage.vue'
-import ProblemUpload from './ProblemUpload.vue'
-import ReviewCarousel from './ReviewCarousel.vue'
-import FindTutor from './FindTutor.vue'
-import { ref } from 'vue'
-import type { Ref } from 'vue'
+import AboutPage from '@/pages/mainpage/AboutPage.vue'
+import ProblemUpload from '@/pages/mainpage/ProblemUpload.vue'
+import ReviewCarousel from '@/pages/mainpage/ReviewCarousel.vue'
+import FindTutor from '@/pages/mainpage/FindTutor.vue'
+import { useUserStore } from '@/store/userStore'
 
-// 추후 로직 구현 필요(비회원 & 학생 / 선생님 회원 구분)
-const isTutor: Ref<boolean> = ref(true)
+const userStore = useUserStore()
 </script>
 
 <style scoped></style>
