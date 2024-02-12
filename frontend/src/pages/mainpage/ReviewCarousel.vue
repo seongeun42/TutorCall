cac576aa1a09aa3bc77a9c6de0f217b660eb9972
반주현 <qkswngus@gmail.com>
2024-02-12 10:46:14 +0900

    Feat: 선생님 MainPage 리뷰 api 연동
    Related to: S10P12A604-300

--- frontend/src/pages/mainpage/ReviewCarousel.vue
+++ frontend/src/pages/mainpage/ReviewCarousel.vue
@@ -1,29 +1,73 @@
 <template>
   <div>
-    <div v-if="isTutor">
+    <div v-if="userStore.isTutor">
       <div class="text-3xl text-center font-black text-neutral-700 mb-5">
         내 강의를 들은 학생들의 리뷰를 확인하세요
       </div>
+      <Carousel :autoplay="2000" :itemsToShow="3.95" :wrapAround="true" :transition="500">
+        <Slide v-for="(slide, index) in tutorReviews" :key="index">
+          <ReviewDetail :data="slide" />
+        </Slide>
+      </Carousel>
     </div>
-    <div v-else class="text-3xl text-center font-black text-neutral-700 mb-5">
-      내가 등록한 리뷰를 확인하세요
+    <div v-if="!userStore.isTutor && userStore.isLogin">
+      <div class="text-3xl text-center font-black text-neutral-700 mb-5">
+        내가 등록한 리뷰를 확인하세요
+      </div>
+      <Carousel :autoplay="2000" :itemsToShow="3.95" :wrapAround="true" :transition="500">
+        <Slide v-for="(slide, index) in myReviews" :key="index">
+          <ReviewDetail :data="slide" />
+        </Slide>
+      </Carousel>
     </div>
-    <Carousel :autoplay="2000" :itemsToShow="3.95" :wrapAround="true" :transition="500">
-      <Slide v-for="(slide, index) in Reviews" :key="index">
-        <ReviewDetail :data="slide" />
-      </Slide>
-    </Carousel>
+    <!-- <div v-else>
+      <div class="text-3xl text-center font-black text-neutral-700 mb-5">
+        비로그인용 더미데이터?
+      </div>
+      <Carousel :autoplay="2000" :itemsToShow="3.95" :wrapAround="true" :transition="500">
+        <Slide v-for="(slide, index) in Reviews" :key="index">
+          <ReviewDetail :data="slide" />
+        </Slide>
+      </Carousel>
+    </div> -->
     <!-- <RouterLink :to="{ name: 'tutorMyLectures', params: { userId: 필요한가? }}" v-if="isTutor" class="more" > 더 보기 > </RouterLink> // 이 부분 선생마이페이지 리뷰 추가되면 작성하기 -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, onMounted } from 'vue'
 import type { Ref } from 'vue'
 import { Carousel, Slide } from 'vue3-carousel'
 import ReviewDetail from './ReviewDetail.vue'
+import { useUserStore } from '@/store/userStore'
+import * as api from '@/api/mainpage/mainpage'
+import { type AxiosResponse } from 'axios'
+import type {reviewerInfo, tutorInfo, tutorReviewInfo, tutorReviewResponse, tagInfo, lectureResponse} from '@/interface/mainpage/interface'
 
 import 'vue3-carousel/dist/carousel.css'
+
+const userStore = useUserStore()
+
+async function init(tutorId: number): Promise<void> {
+  await api.tutorReview(tutorId).then((response: AxiosResponse<tutorReviewResponse>) => {
+    if (response.status == 200) {
+      for(let i = 0; i < response.data.content.length; i++) {
+        const review = response.data.content[i];
+  const reviewer = review.reviewer;
+
+  tutorReviews.value.push({
+    profileUrl: reviewer.profile,
+    nickname: reviewer.nickname,
+    rating: ((review.communicationRate + review.mannerRate + review.professionalismRate) / 3),
+    content: review.content
+  });
+      }
+      // tutorReviews.value = response.data.content.
+      console.log(tutorReviews.value)
+      // console.log(response.data.content[0])
+    }
+  })
+}
 
 defineComponent({
   name: 'AutoPlay',
@@ -35,14 +79,18 @@ defineComponent({
 })
 
 interface Review {
-  reviewId: number
+  // reviewId: number
   profileUrl: string
   nickname: string
   rating: number
   content: string
 }
 
-const Reviews: Ref<Review[]> = ref([
+const tutorReviews: Ref<Review[]> = ref([])
+
+const myReviews: Ref<Review[]> = ref([])
+
+const defaultReviews: Ref<Review[]> = ref([
   {
     reviewId: 1,
     profileUrl: 'src/img/default_profile.png',
@@ -102,7 +150,9 @@ const Reviews: Ref<Review[]> = ref([
   }
 ])
 
-const isTutor: Ref<boolean> = ref(true)
+onMounted(async (): Promise<void> => {
+  await init(1)
+})
 </script>
 
 <style scoped>
