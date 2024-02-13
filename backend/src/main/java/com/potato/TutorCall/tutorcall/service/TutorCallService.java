3b2a2420e437b8f9aafed5c74a170c1e7ee845b2
seongeun <gkapf838@gmail.com>
2024-02-14 01:06:03 +0900

    Feat: Call redis에 저장 및 불러오기

--- backend/src/main/java/com/potato/TutorCall/tutorcall/service/TutorCallService.java
+++ backend/src/main/java/com/potato/TutorCall/tutorcall/service/TutorCallService.java
@@ -1,9 +1,16 @@
 package com.potato.TutorCall.tutorcall.service;
 
+import com.potato.TutorCall.exception.customException.NotFoundException;
+import com.potato.TutorCall.tutorcall.domain.RequestCall;
+import com.potato.TutorCall.tutorcall.domain.ResponseCall;
 import com.potato.TutorCall.tutorcall.domain.TutorCall;
+import com.potato.TutorCall.tutorcall.repository.RequestCallRepository;
+import com.potato.TutorCall.tutorcall.repository.ResponseCallRepository;
 import com.potato.TutorCall.tutorcall.repository.TutorCallRepository;
 import com.potato.TutorCall.user.domain.User;
 import java.util.List;
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class TutorCallService {
 
   private final TutorCallRepository tutorcallRepository;
+  private final RequestCallRepository requestCallRepository;
+  private final ResponseCallRepository responseCallRepository;
 
   @Transactional(readOnly = true)
   public TutorCall findById(Long id) {
@@ -23,6 +32,10 @@ public class TutorCallService {
   @Transactional(readOnly = true)
   public List<TutorCall> findUserTutorCalls(User currentUser) {
     return tutorcallRepository.findByUserOrderByIdDesc(currentUser);
+  }
+
+  public Long save(TutorCall tutorCall) {
+    return tutorcallRepository.save(tutorCall).getId();
   }
 
   public void startTutorCallLive(Long tutorCallId, String sessionId) {
@@ -36,4 +49,28 @@ public class TutorCallService {
     tutorCall.changeLiveState(false);
     tutorCall.setLiveUrl(null);
   }
+
+  public void tutorCallRequestSave(RequestCall requestCall) {
+    requestCallRepository.save(requestCall);
+  }
+
+  public void tutorCallResponseSave(UUID requestId, ResponseCall responseCall) {
+    updateAnswerCount(requestId);
+    responseCallRepository.save(responseCall);
+  }
+
+  public RequestCall getRequestCall(UUID id) {
+    return requestCallRepository.findById(id).orElseThrow(() -> new NotFoundException("존재하지 않는 요청입니다."));
+  }
+
+  public ResponseCall getResponseCall(UUID id) {
+    return responseCallRepository.findById(id).orElseThrow(() -> new NotFoundException("존재하지 않는 요청입니다."));
+  }
+
+  private void updateAnswerCount(UUID id) {
+    RequestCall requestCall = this.getRequestCall(id);
+    requestCall.increaseTutorCount();
+    requestCallRepository.save(requestCall);
+  }
+
 }
