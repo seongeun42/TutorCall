ac503ac25c8d7b4d0bbbf8e76bbdd07e986aa455
seongeun <gkapf838@gmail.com>
2024-02-14 01:04:53 +0900

    Feat: Call Repository 생성

--- backend/src/main/java/com/potato/TutorCall/tutorcall/repository/RequestCallRepository.java
+++ backend/src/main/java/com/potato/TutorCall/tutorcall/repository/RequestCallRepository.java
@@ -1,8 +1,10 @@
 package com.potato.TutorCall.tutorcall.repository;
 
 import com.potato.TutorCall.tutorcall.domain.RequestCall;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface RequestCallRepository extends JpaRepository<RequestCall, Long> {}
+public interface RequestCallRepository extends CrudRepository<RequestCall, UUID> {}
