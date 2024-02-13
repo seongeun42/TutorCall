ba3a215d0d1f57eabbe074e496f2adca2198832d
sikutisa <ilgooinkorea@gmail.com>
2024-02-14 00:10:02 +0900

    Feat: add chat domains and dtos

---
+++ backend/src/main/java/com/potato/TutorCall/chat/domain/ChatParticipants.java
@@ -0,0 +1,26 @@
+package com.potato.TutorCall.chat.domain;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ChatParticipants {
+  @Id
+  String id;
+
+  Long userId;
+
+  String chatroomId;
+
+  LocalDateTime lastVisited = LocalDateTime.now();
+
+  @Builder
+  public ChatParticipants(String id, Long userId, String chatroomId) {
+    this.id = id;
+    this.userId = userId;
+    this.chatroomId = chatroomId;
+  }
+}
