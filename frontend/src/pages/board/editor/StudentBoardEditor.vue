837cd64ff89cec927d774e032b9e0bcdaa2da918
박연서 <qkrdustj45@naver.com>
2024-02-15 11:07:33 +0900

    Fix: conflict

--- frontend/src/pages/board/editor/StudentBoardEditor.vue
+++ frontend/src/pages/board/editor/StudentBoardEditor.vue
@@ -52,7 +52,7 @@ watch(
       subjectDisabled.value = true
       subjectSelected.value = ''
     }
-    if(editStore.needEdit) gradeSelected.value = editStore.grade.toString();
+    if(editStore.needEdit) gradeSelected.value = editStore.grade.toString()
   }
 )
 
